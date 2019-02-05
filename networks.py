d095bea57d99cb1d9330b2bb82f3839e6099c3da
浅野一真 <kazuma@asanokazumanoMacBook-Pro.local>
2019-02-05 16:01:39 +0900

    update2

--- networks.py
+++ networks.py
@@ -17,6 +17,35 @@ def get_output_ch(resolution):
 
     return ch
 
+################################################################################
+class VGG19FeatureExtractor(nn.Module):
+    def __init__(self):
+        super(VGG19FeatureExtractor, self).__init__()
+        vgg16 = models.vgg19(pretrained=True)
+
+        self.enc_1 = nn.Sequential(*vgg16.features[:5])
+        self.enc_2 = nn.Sequential(*vgg16.features[5:10])
+        self.enc_3 = nn.Sequential(*vgg16.features[10:19])
+        self.enc_4 = nn.Sequential(*vgg16.features[19:28])
+        self.enc_5 = nn.Sequential(*vgg16.features[28:37])
+
+        print(self.enc_1)
+        print(self.enc_2)
+        print(self.enc_3)
+        print(self.enc_4)
+        print(self.enc_5)
+
+        # fix the encoder
+        for i in range(3):
+            for param in getattr(self, 'enc_{:d}'.format(i + 1)).parameters():
+                param.requires_grad = False
+
+    def forward(self, image):
+        results = [image]
+        for i in range(3):
+            func = getattr(self, 'enc_{:d}'.format(i + 1))
+            results.append(func(results[-1]))
+        return results[1:]
 
 class CRN(nn.Module):
     def __init__(self, super_resolution=256, groups=6):
